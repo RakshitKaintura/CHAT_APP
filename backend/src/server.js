a07610012450dad603562ae847d06997a594200f
RakshitKaintura <blank1951k@gmail.com>
2026-01-22 20:06:53 +0530

    Update Profile Pic

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,23 @@
 import express from "express";
 import path from "path";
-
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js"; // 1. Import your DB connection utility
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { ENV } from "./lib/env.js";
 
-    
+
 
 const app = express();
 const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json()); 
-
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(ENV.NODE_ENV === "production"){
+if (ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
     app.get("*", (_, res) => {
