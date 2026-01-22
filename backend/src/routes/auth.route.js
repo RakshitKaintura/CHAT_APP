0564e6a31328fb046cf29181b09fc90811300fa4
RakshitKaintura <blank1951k@gmail.com>
2026-01-22 18:00:11 +0530

    Login and Logout Added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,20 +1,10 @@
-
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-// CHANGE THIS: signup should be a POST request to accept body data
 router.post("/signup", signup);
-
-// CHANGE THIS: login should usually be POST to keep credentials out of the URL
-router.post("/login", (req, res) => {
-    res.send("Login Successful");
-});
-
-// FIX TYPO: removed the space after "/logout "
-router.get("/logout", (req, res) => {
-    res.send("Logout Successful");
-});
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
