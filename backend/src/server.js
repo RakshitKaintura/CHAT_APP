689834efc2295ecc87fed72db15820202859e4e3
RakshitKaintura <blank1951k@gmail.com>
2026-01-24 21:11:16 +0530

    Chat Page Implementation

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
-app.use(express.json());
+app.use(express.json({limit:"15mb"}));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
